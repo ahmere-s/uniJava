ff8e282a4d7176aa5f96a4227329aaacb4c7641b
ahmere-s <csmail.santos@gmail.com>
2026-02-15 22:43:23 -0500

    example

---
+++ src/com/lab/utils/Main.java
@@ -0,0 +1,14 @@
+package com.lab.utils;
+import com.lab.utils.matrixUtils;
+
+public class Main {
+	public static void main(String[] args){
+		//System.out.println(version);
+		
+		int[][] example = {{1, 2, 3}, {4, 5, 6}};
+		
+		matrixUtils haha = new matrixUtils();
+		
+		haha.printMatrix(example);
+	}
+}
